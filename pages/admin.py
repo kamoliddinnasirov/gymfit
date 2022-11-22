60b15337a1988de2a2f21b68c90d6724805bee2f
Acer <kamoliddinnasirov@mail.ru>
2022-11-22 19:51:35 +0500

    all models add in all applications

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from pages.models import PopularTrainer, AboutBanner, GalleryMain, Gallery
+from django.contrib.admin import TabularInline
+
+
+from pages.models import PopularTrainer, AboutBanner, GalleryMain, Gallery, TrainerBanner, Weeks, Category, OurTrainer, \
+    CourseBanner, PopularCourseGallery, PopularCourse, Mentor, TrainerGallery
+
 
 @admin.register(PopularTrainer)
 class PopularTrainerAdmin(admin.ModelAdmin):
@@ -8,6 +13,7 @@ class PopularTrainerAdmin(admin.ModelAdmin):
     search_fields = ('people_about',)
     list_filter = ('created_at',)
 
+
 @admin.register(AboutBanner)
 class AboutBannerAdmin(admin.ModelAdmin):
     list_display = ('about_title', 'created_at')
@@ -15,8 +21,9 @@ class AboutBannerAdmin(admin.ModelAdmin):
     list_filter = ('created_at',)
 
 
-class ImageInline(admin.TabularInline): #TabularInline ForeignKey bilan birgalikda ishlidi
+class ImageInline(admin.TabularInline):  # TabularInline ForeignKey bilan birgalikda ishlidi
     model = Gallery
+
 
 @admin.register(GalleryMain)
 class GalleryMainAdmin(admin.ModelAdmin):
@@ -29,12 +36,60 @@ class GalleryMainAdmin(admin.ModelAdmin):
     list_filter = ('created_at',)
 
 
+@admin.register(TrainerBanner)
+class TrainerBannerAdmin(admin.ModelAdmin):
+    list_display = ('trainer_title',)
+    list_display_links = ('trainer_title',)
+    list_filter = ('created_at',)
 
 
+@admin.register(Weeks)
+class WeekAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
 
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+    list_filter = ('created_at',)
 
 
+class TrainerGalleryAdmin(TabularInline):
+    model = TrainerGallery
+
+@admin.register(OurTrainer)
+class OurTrainerAdmin(admin.ModelAdmin):
+    inlines = [
+        TrainerGalleryAdmin,
+    ]
+    list_display = ('full_name', 'category')
+    list_display_links = ('full_name', 'category')
+    search_fields = ('full_name', 'category')
 
 
+@admin.register(CourseBanner)
+class CourseBannerAdmin(admin.ModelAdmin):
+    list_display = ('courses_title',)
+    list_display_links = ('courses_title',)
+    list_filter = ('created_at',)
 
+
+class CourseGallery(TabularInline):
+    model = PopularCourseGallery
+
+
+class MentorAdmin(TabularInline):
+    model = Mentor
+
+
+@admin.register(PopularCourse)
+class PopCourse(admin.ModelAdmin):
+    list_display = ('course',)
+    list_display_links = ('course',)
+    inlines = [
+        MentorAdmin,
+        CourseGallery,
+
+    ]
--- pages/models.py
+++ pages/models.py
@@ -6,7 +6,6 @@ import datetime
 from django.core.validators import MaxValueValidator
 
 
-
 # ================================About==================
 class AboutBanner(BaseModel):
     about_title = models.CharField(max_length=20, null=True, blank=True, verbose_name=_('about title'))
@@ -52,9 +51,10 @@ class GalleryMain(BaseModel):
         verbose_name = _('gallery main')
         verbose_name_plural = _('gallery main')
 
+
 class Gallery(BaseModel):
     gallery = models.ForeignKey(GalleryMain, on_delete=models.RESTRICT)
-    image = models.ImageField(upload_to='about_gallery/%Y/%m/%d/', verbose_name=_('image'))
+    image = models.ImageField(upload_to='about_gallery/%Y/%m/%d/', default='some_value', verbose_name=_('image'))
     is_active = models.BooleanField(default=False, verbose_name=_('is_active'))
 
     def __str__(self):
@@ -71,7 +71,7 @@ class Gallery(BaseModel):
 class TrainerBanner(BaseModel):
     trainer_title = models.CharField(max_length=20, null=True, blank=True, verbose_name=_('trainer title'))
     trainer_banner = models.ImageField(upload_to='trainer_banner/%Y/%m/%d/', null=True, blank=True,
-                                     verbose_name=_('trainer banner'))
+                                       verbose_name=_('trainer banner'))
 
     def __str__(self):
         return self.trainer_title
@@ -80,6 +80,28 @@ class TrainerBanner(BaseModel):
         verbose_name = _('trainer banner')
         verbose_name_plural = _('trainer banners')
 
+class Weeks(BaseModel):
+    name = models.CharField(max_length=50, verbose_name=_('name'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('week')
+        verbose_name_plural = _('weeks')
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50, verbose_name=_("name"))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+
 class OurTrainer(BaseModel):
     full_name = models.CharField(max_length=255, verbose_name=_('full name'))
     category = models.ForeignKey(Category, on_delete=models.RESTRICT, verbose_name=_('category'))
@@ -87,7 +109,7 @@ class OurTrainer(BaseModel):
     facebook = models.URLField()
     twitter = models.URLField()
     linkedin = models.URLField()
-    image = models.ImageField(upload_to='trainer_photo/%Y/%m/%d/', verbose_name=_('image'))
+    # image = TubalarInline
 
     def __str__(self):
         return f"{self.full_name}, {self.category}"
@@ -95,4 +117,116 @@ class OurTrainer(BaseModel):
     class Meta:
         verbose_name = _('Our trainer')
         verbose_name_plural = _('Our trainer')
+
+class TrainerGallery(BaseModel):
+    gallery = models.ForeignKey(OurTrainer, on_delete=models.RESTRICT, verbose_name=_('gallery'))
+    image = models.ImageField(upload_to='TrainerGallery/', default='some_value')
+    is_active = models.BooleanField(default=False, verbose_name=_('is_active'))
+
+    def __str__(self):
+        return self.gallery
+
+    class Meta:
+        verbose_name = _("gallery")
+        verbose_name_plural = _('gallery')
+
+
+class CourseBanner(BaseModel):
+    courses_title = models.CharField(max_length=20, null=True, blank=True, verbose_name=_('courses title'))
+    courses_banner = models.ImageField(upload_to='courses_banner/%Y/%m/%d/', null=True, blank=True,
+                                       verbose_name=_('courses banner'))
+
+    def __str__(self):
+        return self.courses_title
+
+    class Meta:
+        verbose_name = _('course banner')
+        verbose_name_plural = _('courses banner')
+
+
+class PopularCourse(BaseModel):
+    # title = models.CharField(max_length=255, null=True, blank=True, verbose_name=_('title'))
+    course = models.ForeignKey(Category, on_delete=models.RESTRICT, verbose_name=_('course'))
+    # mentor = TabularInline
+    weeks = models.ManyToManyField(Weeks, verbose_name=_('weeks'))
+    from_time = models.TimeField(auto_now=False, verbose_name=_('from time'))
+    to_time = models.TimeField(auto_now=False, verbose_name=_('to time'))
+    # main_banner = TabularInline
+
+    def __str__(self):
+        return f"{self.weeks}"
+
+    class Meta:
+        verbose_name = _('popular course')
+        verbose_name_plural = _('popular courses')
+
+
+class Mentor(BaseModel): # admin.py TabularInline uchun
+    name = models.ForeignKey(PopularCourse, on_delete=models.RESTRICT, verbose_name=_('name'))
+    full_name = models.CharField(max_length=50, verbose_name=_('full_name'))
+    is_active = models.BooleanField(default=False, verbose_name=_('is_active'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("mentor")
+        verbose_name_plural = _('mentors')
+
+
+class PopularCourseGallery(BaseModel):  # admin.py TabularInline uchun
+    gallery = models.ForeignKey(PopularCourse, on_delete=models.RESTRICT,  verbose_name=_('gallery'))
+    image = models.ImageField(upload_to='PopularCourseGallery/', default='some_value')
+    is_active = models.BooleanField(default=False, verbose_name=_('is_active'))
+
+    def __str__(self):
+        return self.gallery
+
+    class Meta:
+        verbose_name = _("gallery")
+        verbose_name_plural = _('gallery')
+
+
+# =========================Course Single==================
+
+class CourseSingleBanner(BaseModel):
+    course_single_title = models.CharField(max_length=50, null=True, blank=True, verbose_name=_('course_single_title'))
+    course_single_banner = models.ImageField(upload_to='courses_single_banner/%Y/%m/%d/', null=True, blank=True,
+                                       verbose_name=_('course_single_banner'))
+
+    def __str__(self):
+        return self.course_single_title
+
+    class Meta:
+        verbose_name = _('Course Single Banner')
+        verbose_name_plural = _('Course Single Banners')
+
+class CSingle(BaseModel):
+    hours = models.CharField(max_length=50, verbose_name=_('hours'))
+    calories_count = models.PositiveIntegerField(verbose_name=_('calories count'))
+    type_calories = models.CharField(max_length=50, verbose_name=_('type calories'))
+    workout_count = models.PositiveIntegerField(verbose_name=_('workout count (%)'))
+    type_workout = models.CharField(max_length=50, verbose_name=_('type workout'))
+    body = models.TextField(verbose_name=_('body'))
+
+    def __str__(self):
+        return f"{self.type_workout}"
+
+    class Meta:
+        verbose_name = _('Course Single')
+        verbose_name_plural = _('Course Single')
+
+class Feature(BaseModel):
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT, verbose_name=_('category'))
+    duration = models.PositiveIntegerField(verbose_name=_('duration'))
+    students = models.PositiveIntegerField(verbose_name=_('students'))
+    shift = models.CharField(max_length=50, verbose_name=_('shift'))
+    price = models.PositiveIntegerField(verbose_name=_('price'))
+
+    def __str__(self):
+        return  f"{self.category}, {self.students}, {self.price}"
+
+    class Meta:
+        verbose_name = _('Feature')
+        verbose_name_plural = _('Features')
 
